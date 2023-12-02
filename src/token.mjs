a636d1a2dbd9f74908d7d01918090fb0a398f717
Monir <monireh.taghilo@gmail.com>
2023-12-02 09:57:51 +0330

    initialize react app

---
+++ src/token.mjs
@@ -0,0 +1,10 @@
+import { Octokit } from "octokit";
+
+const token = "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";
+
+const octokit = new Octokit({ auth: token });
+
+const {
+  data: { login },
+} = await octokit.rest.users.getAuthenticated();
+console.log("Hello, %s", login);
